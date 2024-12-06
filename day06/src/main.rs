9e78da0bb311469f3baca18edf1b50ad913bdd35
rbaron <rbaron@rbaron.net>
2024-12-06 14:55:46 +0100

    Day 05 optimization
    Part 2 -- we only need to check previous states when there's a turn.
    Runtime 20s -> 3s

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -31,18 +31,11 @@ impl Dir {
     }
 }
 
-fn find_exit(pos: &Pos, dir: Dir, grid: &HashMap<Pos, char>) -> Result<HashSet<Pos>, ()> {
+fn find_exit(pos: &Pos, dir: Dir, grid: &HashMap<Pos, char>) -> HashSet<Pos> {
     let mut pos = *pos;
     let mut dir = dir;
-    let mut visited: HashSet<(Pos, Dir)> = HashSet::new();
     let mut positions: HashSet<Pos> = HashSet::new();
-    let obs = (6, 3);
     loop {
-        // Error if there's a loop.
-        if visited.contains(&(pos, dir)) {
-            return Err(());
-        }
-        visited.insert((pos, dir));
         positions.insert(pos);
         let next_pos = dir.step(&pos);
         match grid.get(&next_pos) {
@@ -50,7 +43,33 @@ fn find_exit(pos: &Pos, dir: Dir, grid: &HashMap<Pos, char>) -> Result<HashSet<P
                 dir = dir.turn_right();
             }
             None => {
-                return Ok(positions);
+                return positions;
+            }
+            Some(_) => {
+                pos = next_pos;
+            }
+        }
+    }
+}
+
+fn find_exit_pt2(pos: &Pos, dir: Dir, grid: &HashMap<Pos, char>) -> Result<(), ()> {
+    let mut pos = *pos;
+    let mut dir = dir;
+    let mut visited: HashSet<(Pos, Dir)> = HashSet::new();
+    loop {
+        let next_pos = dir.step(&pos);
+        match grid.get(&next_pos) {
+            Some('#') => {
+                dir = dir.turn_right();
+                if visited.contains(&(pos, dir)) {
+                    // Loop detected.
+                    return Ok(());
+                }
+                visited.insert((pos, dir));
+            }
+            None => {
+                // Out of bounds.
+                return Err(());
             }
             Some(_) => {
                 pos = next_pos;
@@ -77,19 +96,20 @@ fn main() {
     let initial_pos = *grid.iter().find(|(_, &c)| c == '^').unwrap().0;
 
     grid.insert(initial_pos, '.');
-    let visited = find_exit(&initial_pos, Dir::Up, &grid).unwrap();
+    let visited = find_exit(&initial_pos, Dir::Up, &grid);
     println!("{}", visited.len());
 
     let mut n_loops = 0;
     for maybe_obst in visited {
         grid.insert(maybe_obst, '#');
-        match find_exit(&initial_pos, Dir::Up, &grid) {
-            Err(_) => {
+        match find_exit_pt2(&initial_pos, Dir::Up, &grid) {
+            Ok(_) => {
                 n_loops += 1;
             }
-            Ok(_) => {}
+            Err(_) => {}
         }
         grid.insert(maybe_obst, '.');
     }
+
     println!("{}", n_loops);
 }
