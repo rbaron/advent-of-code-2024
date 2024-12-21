641850f0110ca3803e80b72b126a9a3d2fbcca7f
rbaron <rbaron@rbaron.net>
2024-12-22 00:28:03 +0100

    Day 21 some progress?

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -26,8 +26,6 @@ impl Pos {
 
     fn diff(&self, other: &Pos) -> Pos {
         Pos {
-            // x: other.x - self.x,
-            // y: other.y - self.y,
             x: self.x - other.x,
             y: self.y - other.y,
         }
@@ -53,39 +51,9 @@ impl Pos {
             v.push('A');
             res.push(v);
         }
-
-        // if moves.len() == 1 {
-        //     moves.push('A');
-        //     res.push(moves);
-        //     return res;
-        // }
-        // let mut clone = moves.clone();
-        // clone.push('A');
-        // res.push(clone);
-        // moves.reverse();
-        // moves.push('A');
-        // res.push(moves);
-
         res
     }
 }
-
-// fn count_steps(
-//     digits: Vec<char>,
-//     keymap: &HashMap<char, Pos>,
-//     sortmap: &HashMap<char, Pos>,
-// ) -> Vec<char> {
-//     let mut pos = keymap.get(&'A').unwrap();
-//     let mut steps = Vec::new();
-//     for digit in digits {
-//         let new_pos = keymap.get(&digit).unwrap();
-//         let diff = pos.diff(new_pos);
-//         let moves = diff.to_moves(sortmap);
-//         steps.extend(&moves);
-//         pos = new_pos;
-//     }
-//     steps
-// }
 
 fn validate_moves(pos: &Pos, moves: &Moves, allowed: &HashSet<Pos>) -> bool {
     let mut pos = pos.clone();
@@ -147,6 +115,53 @@ fn get_moves(
     res
 }
 
+fn get_moves2(
+    from: char,
+    to: char,
+    level: i32,
+    max_level: i32,
+    keymap: &HashMap<char, Pos>,
+    allowed: &HashSet<Pos>,
+    cache: &mut HashMap<(i32, (char, char)), Vec<Moves>>,
+) -> Vec<Moves> {
+    // println!("{:?} {:?}", from, to);
+    let key = (level, (from, to));
+    if cache.contains_key(&key) {
+        return cache.get(&key).unwrap().clone();
+    }
+
+    let mut res = Vec::new();
+
+    let from_pos = keymap.get(&from).unwrap();
+    let to_pos = keymap.get(&to).unwrap();
+    let diff = to_pos.diff(from_pos);
+    let moves = diff.to_moves(&allowed);
+
+    if level == max_level {
+        return moves;
+    }
+
+    for m in moves {
+        let mut with_a = vec!['A'];
+        with_a.extend(&m);
+        for pair in with_a.iter().as_slice().windows(2) {
+            let (from, to) = (pair[0], pair[1]);
+            let sub_moves: Vec<Moves> =
+                get_moves2(from, to, level + 1, max_level, keymap, allowed, cache);
+            for sub in sub_moves.iter().unique() {
+                let mut full = m.clone();
+                full.extend(sub);
+                res.push(full);
+            }
+            if level == 0 {
+                println!("{:?} {:?}", res.len(), res);
+            }
+        }
+    }
+    cache.insert(key, res.clone());
+    res
+}
+
 fn main() {
     let KEYPAD = HashMap::from([
         ('7', Pos { y: 0, x: 0 }),
@@ -176,99 +191,146 @@ fn main() {
     let inputs = std::fs::read_to_string(filename).unwrap();
 
     let mut res: i32 = 0;
-    // let input = "456A";
+
+    // let input = "029A";
+    // let input = "029A";
     for input in inputs.split('\n') {
         let chars = input.chars().collect::<Vec<char>>();
         let moves1 = get_moves(&chars, 'A', &KEYPAD, &KEYPAD_ALLOWED, &mut HashMap::new());
-        for m in moves1.iter().unique() {
-            // println!("{:?}", m.len());
-        }
-        println!("move1 {:?}", moves1.len());
 
-        let moves2: Vec<Moves> = moves1
-            .iter()
-            .unique()
-            .flat_map(|m| get_moves(m, 'A', &ROBOT, &ROBOT_ALLOWED, &mut HashMap::new()))
-            .collect();
-        println!("move2 {:?}", moves2.len());
-        // for m in moves2.iter().unique() {
-        //     println!("{:?} {:?}", m.len(), m);
-        // }
-        // break;
+        // println!("moves1 {:?}", moves1);
 
-        // let moves2 = vec![moves2.iter().min_by_key(|m| m.len()).unwrap()];
-        let l = moves2.iter().map(|m| m.len()).min().unwrap();
+        // let move1 = &moves1[0];
 
-        // let blks: Vec<Vec<Vec<Moves>>> = moves2
-        //     .iter()
-        //     .map(|m| {
-        //         m.iter()
-        //             .cloned()
-        //             .collect::<Vec<char>>()
-        //             .split(|&c| c == 'A')
-        //             .map(|s| s.to_vec())
-        //             .collect()
-        //     })
-        //     .collect();
-        let blks: Vec<Vec<Moves>> = moves2
-            .iter()
-            .map(|m| {
-                m.iter()
-                    .cloned()
-                    .collect::<Vec<char>>()
-                    .split(|&c| c == 'A')
-                    .map(|s| s.to_vec())
-                    .collect()
-            })
-            .collect();
-
-        // for blk in blks.iter() {
-        //     for m in blk.iter() {
-        //         println!("{:?} {:?}", m.len(), m);
-        //     }
-        //     println!();
-        // }
-
-        let mut cache: HashMap<String, i32> = HashMap::new();
-        let mut min = i32::MAX;
-        for blk in blks.iter().unique() {
-            let mut size = 0;
-            for m in blk.iter() {
-                let key = m.iter().collect::<String>();
-                if cache.contains_key(&key) {
-                    size += cache.get(&key).unwrap();
-                    // println!("skip {:?}", key);
-                    continue;
-                }
-                let mut c = m.clone();
-                c.push('A');
-                let moves = get_moves(&c, 'A', &ROBOT, &ROBOT_ALLOWED, &mut HashMap::new());
+        for move1 in moves1.iter().unique() {
+            let mut with_a = vec!['A'];
+            with_a.extend(move1.clone());
+            let mut cache: HashMap<(i32, (char, char)), Vec<Moves>> = HashMap::new();
+            let mut sum = 0;
+            for pair in with_a.iter().as_slice().windows(2) {
+                let (from, to) = (pair[0], pair[1]);
+                let moves = get_moves2(from, to, 0, 2, &ROBOT, &ROBOT_ALLOWED, &mut cache);
+                // for m in moves.iter().unique() {
+                //     println!("{:?}", m);
+                // }
+                // break;
                 let shortest = moves.iter().min_by_key(|m| m.len()).unwrap();
-                let val = shortest.len() as i32;
-                cache.insert(key, val);
-                size += val;
+                println!("{:?} {:?}", shortest.len(), shortest);
+                break;
+                sum += shortest.len();
             }
-            if size < min {
-                min = size;
-            }
+            println!("{}, sum {:?}", &input, sum);
+            break;
         }
 
-        println!("MIN {:?}", min - 1);
-
-        // let moves3: Vec<Moves> = moves2
-        //     .iter()
-        //     .filter(|m| m.len() == l)
-        //     .flat_map(|m| get_moves(m, 'A', &ROBOT, &ROBOT_ALLOWED, &mut HashMap::new()))
-        //     .collect();
-        // println!("{:?}", moves3.len());
-
-        // let shortest = moves3.iter().min_by_key(|m| m.len()).unwrap();
-        // println!("{:?} {:?}", input, shortest.len());
-        // println!("{:?} {:?}", input, shortest.iter().collect::<String>());
-        // break;
-
-        // res += (input[..input.len() - 1].parse::<i32>().unwrap() * shortest.len() as i32);
-        res += (input[..input.len() - 1].parse::<i32>().unwrap() * (min - 1) as i32);
+        break;
     }
-    println!("RES {}", res);
+
+    // let input = "456A";
+    // for input in inputs.split('\n') {
+    // let chars = input.chars().collect::<Vec<char>>();
+    // let moves1 = get_moves(&chars, 'A', &KEYPAD, &KEYPAD_ALLOWED, &mut HashMap::new());
+    // for m in moves1.iter().unique() {
+    //     // println!("{:?}", m.len());
+    // }
+    // println!("move1 {:?}", moves1.len());
+
+    // let moves2: Vec<Moves> = moves1
+    //     .iter()
+    //     .unique()
+    //     .flat_map(|m| get_moves(m, 'A', &ROBOT, &ROBOT_ALLOWED, &mut HashMap::new()))
+    //     .collect();
+    // println!("move2 {:?}", moves2.len());
+    // for m in moves2.iter().unique() {
+    //     println!("{:?} {:?}", m.len(), m);
+    // }
+    // break;
+
+    // let moves2 = vec![moves2.iter().min_by_key(|m| m.len()).unwrap()];
+    // let l = moves2.iter().map(|m| m.len()).min().unwrap();
+
+    // let blks: Vec<Vec<Vec<Moves>>> = moves2
+    //     .iter()
+    //     .map(|m| {
+    //         m.iter()
+    //             .cloned()
+    //             .collect::<Vec<char>>()
+    //             .split(|&c| c == 'A')
+    //             .map(|s| s.to_vec())
+    //             .collect()
+    //     })
+    //     .collect();
+    // let blks: Vec<Vec<Moves>> = moves1
+    //     .iter()
+    //     .map(|m| {
+    //         m.iter()
+    //             .cloned()
+    //             .collect::<Vec<char>>()
+    //             .split(|&c| c == 'A')
+    //             .map(|s| s.to_vec())
+    //             .collect()
+    //     })
+    //     .collect();
+
+    // let mut cache: HashMap<(usize, String), i32> = HashMap::new();
+    // let mut min = i32::MAX;
+    // for blk in blks.iter().unique() {
+    //     let mut size = 0;
+    //     for m in blk.iter() {
+    //         for i in 0..2 {
+    //         let key = (i, m.iter().collect::<String>());
+    //         if cache.contains_key(&key) {
+    //             size += cache.get(&key).unwrap();
+    //             // println!("skip {:?}", key);
+    //             continue;
+    //         }
+    //         let mut c = m.clone();
+    //         c.push('A');
+    //         let moves = get_moves(&c, 'A', &ROBOT, &ROBOT_ALLOWED, &mut HashMap::new());
+    //         let shortest = moves.iter().min_by_key(|m| m.len()).unwrap();
+    //         let val = shortest.len() as i32;
+    //         cache.insert(key, val);
+    //         size += val;
+    //     }
+    //     if size < min {
+    //         min = size;
+    //     }
+    // }
+
+    // for blk in blks.iter() {
+    //     for m in blk.iter() {
+    //         println!("{:?} {:?}", m.len(), m);
+    //     }
+    //     println!();
+    // }
+
+    // let mut cache: HashMap<String, i32> = HashMap::new();
+    // let mut min = i32::MAX;
+    // for blk in blks.iter().unique() {
+    //     let mut size = 0;
+    //     for m in blk.iter() {
+    //         let key = m.iter().collect::<String>();
+    //         if cache.contains_key(&key) {
+    //             size += cache.get(&key).unwrap();
+    //             // println!("skip {:?}", key);
+    //             continue;
+    //         }
+    //         let mut c = m.clone();
+    //         c.push('A');
+    //         let moves = get_moves(&c, 'A', &ROBOT, &ROBOT_ALLOWED, &mut HashMap::new());
+    //         let shortest = moves.iter().min_by_key(|m| m.len()).unwrap();
+    //         let val = shortest.len() as i32;
+    //         cache.insert(key, val);
+    //         size += val;
+    //     }
+    //     if size < min {
+    //         min = size;
+    //     }
+    // }
+
+    // println!("MIN {:?}", min - 1);
+
+    // res += (input[..input.len() - 1].parse::<i32>().unwrap() * (min - 1) as i32);
+    // }
+    // println!("RES {}", res);
 }
